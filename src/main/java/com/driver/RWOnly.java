6caf35db60e91a1775f54badaad61be5afae149f
SRINIVASAN N R <srinivasannr99@gmail.com>
2023-01-13 11:15:03 +0530

    encapsulation1

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class RWOnly {
+    private String name;
+
+    public String getter(){
+        return name;
+    }
+    public void Setter(String str){
+        this.name=str;
+    }
     
 }
