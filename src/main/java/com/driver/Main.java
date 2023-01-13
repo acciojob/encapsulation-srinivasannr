8ae791046d88f977da962c1a3f7f22a8357b1380
SRINIVASAN N R <srinivasannr99@gmail.com>
2023-01-13 11:24:07 +0530

    encapsulation3

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,8 +3,8 @@ package com.driver;
 public class Main {
     public static void main(String[] args){
         RWOnly obj=new RWOnly();
-        obj.name="srinu";
-        System.out.println(obj.name);
+       // obj.name="srinu";
+        //System.out.println(obj.name);
         obj.setName("srinu");
         System.out.println(obj.getName());
     }
